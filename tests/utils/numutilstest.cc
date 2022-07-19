a3700b293f29ddea0b599403a06462476b0c9653
aaaaluki <luki00.lrb@gmail.com>
2022-07-19 23:07:26 +0200

    Tests: numutils changed ASSERT_ to EXPECT_
    Changed ASSERT_ to EXPECT_, this way the test continues if one line
    fails.
    
    Also added and organized a bit the test

--- tests/utils/numutilstest.cc
+++ tests/utils/numutilstest.cc
@@ -4,15 +4,18 @@ extern "C" {
 #include "utils/numutils.h"
 }
 
-TEST(numutils, positive_ints) {
-  ASSERT_EQ(next_pow2(3), 4);
-  ASSERT_EQ(next_pow2(15), 16);
-  ASSERT_EQ(next_pow2(17), 32);
-  ASSERT_EQ(next_pow2(0), 0);
+TEST(next_pow2, HandlesPositiveInput) {
+  EXPECT_EQ(next_pow2(3), 4);
+  EXPECT_EQ(next_pow2(15), 16);
+  EXPECT_EQ(next_pow2(17), 32);
 }
 
-TEST(numutils, negative_ints) {
-  ASSERT_EQ(next_pow2(-3), 0);
-  ASSERT_EQ(next_pow2(-165), 0);
-  ASSERT_EQ(next_pow2(-1235), 0);
+TEST(next_pow2, HandlesNegativeInput) {
+  EXPECT_EQ(next_pow2(-3), 0);
+  EXPECT_EQ(next_pow2(-165), 0);
+  EXPECT_EQ(next_pow2(-1235), 0);
+}
+
+TEST(next_pow2, HandlesZeroInput) {
+  EXPECT_EQ(next_pow2(0), 0);
 }
