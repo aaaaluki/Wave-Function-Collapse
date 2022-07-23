0559bc380e9688bfb3643970817dd109ae6e82aa
aaaaluki <luki00.lrb@gmail.com>
2022-07-24 00:11:08 +0200

    LibWfc: Moved sample functions out of main()
    Now all the sample functions/utilities are on a separate file (wfc/samples.c)

---
+++ include/wfc/samples.h
@@ -0,0 +1,26 @@
+/**
+ * @file samples.h
+ * @brief This file contains some mini utilities using cairo
+ *
+ */
+
+#ifndef WFC_SAMPLES_H_
+#define WFC_SAMPLES_H_
+
+/**
+ * @brief Gets a folder full of png images and creates a square tile of all the
+ * images. The size of all the tiles must be the same.
+ *
+ * @param argc argc argument from main()
+ * @param argv argv argument from main()
+ * @return int exit code
+ */
+int draw_tiles(int argc, char **argv);
+
+/**
+ * @brief Draws a chessboard with "Hello, world!" written on the center.
+ *
+ */
+void draw_chessboard(void);
+
+#endif // WFC_SAMPLES_H_
